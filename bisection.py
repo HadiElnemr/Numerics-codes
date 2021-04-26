c1827141754a180ccd2b41e32d39b17d9d264b81
Ibrahim Abou Elenein <ibrahimabouelanin@gmail.com>
2021-04-26 02:12:48 +0200

    Refactor the code

--- bisection.py
+++ bisection.py
@@ -1,30 +1,25 @@
 import math
 
+CONST = -0.5307963268 
 
-CONST = -0.5307963268
+f1 = lambda x : x -2*math.sin(x)
+f2 = lambda x : x*x*x-25
+f3 = lambda x : x*x*x+x-4
+f = lambda x : CONST+ math.asin(x) + x*math.sqrt(1-x*x)
 
-
-def f1(x): return x - 2*math.sin(x)
-def f2(x): return x*x*x-25
-def f3(x): return x*x*x+x-4
-def f(x): return CONST + math.asin(x) + x*math.sqrt(1-x*x)
-
-
-def bis(a, b, n):
+def bis(a,b,n):
     c = 0
     for _ in range(n):
         c = (a+b)/2
-        y1 = f(a) # why it's never used?
-        y2 = f(b)
-        y3 = f(c)
+        y1=f(a) # why? not used!
+        y2=f(b)
+        y3=f(c)
         if y3 > 0:
             b = c
         else:
             a = c
     print(c)
-
-def main():
-    bis(float(input('put a\n')), float(input('put b\n')), int(input('put n\n')))
+    
 
 if __name__ == "__main__":
-    main()
+    bis(float(input('put a\n')), float(input('put b\n')),int(input('put n\n')))
