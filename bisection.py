173a28875f7c0bb2121c8106423178a14f5a920c
Ibrahim Abou Elenein <ibrahimabouelanin@gmail.com>
2021-04-26 02:29:14 +0200

    Add arg options
    >python3 bisection.py 0.12 0.10 100, if no argument was presented it
    will ask the use

--- bisection.py
+++ bisection.py
@@ -1,11 +1,12 @@
 import math
+import sys 
 
 CONST = -0.5307963268 
 
-f1 = lambda x : x -2*math.sin(x)
-f2 = lambda x : x*x*x-25
-f3 = lambda x : x*x*x+x-4
-f = lambda x : CONST+ math.asin(x) + x*math.sqrt(1-x*x)
+f1 = lambda x : x - 2 * math.sin(x)
+f2 = lambda x : x * x * x - 25
+f3 = lambda x : x * x * x + x - 4
+f  = lambda x : CONST + math.asin(x) + x * math.sqrt(1-x * x)
 
 def bis(a,b,n):
     c = 0
@@ -22,4 +23,12 @@ def bis(a,b,n):
     
 
 if __name__ == "__main__":
-    bis(float(input('put a\n')), float(input('put b\n')),int(input('put n\n')))
+    if len(sys.argv) < 2:
+        a = float(input('put a: \n'))
+        b = float(input('put b: \n'))
+        c = int(input('put c: \n'))
+    else:
+        a = float(sys.argv[1])    
+        b = float(sys.argv[2])    
+        c = int(sys.argv[3])    
+    bis(a,b,c)    
